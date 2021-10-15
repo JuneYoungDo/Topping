b932766c62b9c0310b8212f399a5720cd835292f
juneyoungdo <ehwnsdud1004@naver.com>
2021-10-15 17:20:39 +0900

    [Topping_01]Sign up

---
+++ src/main/java/com/teenteen/topping/user/VO/User.java
@@ -0,0 +1,32 @@
+package com.teenteen.topping.user.VO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+import javax.validation.Valid;
+import javax.validation.constraints.Email;
+import java.util.Date;
+
+@Entity
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    private String email;
+    private String password;
+    private String gender;
+    private int age;
+    @Column(updatable = false)
+    private String birth;
+    private int level;
+    private String nickname;
+    private String refreshToken;
+    private boolean isDeleted;
+    private Date createdAt;
+}
