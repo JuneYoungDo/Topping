9453aa5d358345b1bb1457099011742db5bad341
juneyoungdo <ehwnsdud1004@naver.com>
2021-11-02 22:52:55 +0900

    [Topping_16]upload video with s3 service

--- src/main/java/com/teenteen/topping/utils/S3Service.java
+++ src/main/java/com/teenteen/topping/utils/S3Service.java
@@ -1,7 +1,48 @@
 package com.teenteen.topping.utils;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Service
+@RequiredArgsConstructor
 public class S3Service {
+    private AmazonS3 s3Client;
+
+    @Value("${cloud.aws.credentials.accessKey}")
+    private String accessKey;
+    @Value("${cloud.aws.credentials.secretKey}")
+    private String secretKey;
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    public String upload(MultipartFile file) throws IOException {
+        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+        s3Client = AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(region)
+                .build();
+
+        String fileName = System.currentTimeMillis() + file.getOriginalFilename();
+
+        ObjectMetadata metadata = new ObjectMetadata();
+        s3Client.putObject(new PutObjectRequest(bucket, fileName, file.getInputStream(), metadata)
+                .withCannedAcl(CannedAccessControlList.PublicRead)
+        );
+
+        return fileName;
+    }
 }
